0aa3a1d68459938e9e338d3ffd5753276c047241
HelloKol <emonhasan20002000@gmail.com>
2022-01-29 00:17:49 +0000

    new

--- src/components/Home page/About.js
+++ src/components/Home page/About.js
@@ -6,11 +6,11 @@ export default function About() {
       <span style={{ color: "#7A7A7A" }}>/about me</span>
       <h1 className="mt-6 text-xl lg:text-3xl xl:text-4xl">
         Hi, I am Shehab Emon. Located in London, United kingdom. I am currently
-        in my final year of university studying computer science. I am looking
-        for freelancing job as a designer or an entry level job for web
-        development. I am familiar with the following technology&nbsp;
+        in my final year of university studying computer science, looking for
+        junior level job as a UI designer or front-end web developer. I am
+        familiar with the following technology&nbsp;
         <span className="skills">
-          HTML - CSS3 - SCSS - Tailwind - Javascript - React - Firebase - Git -
+          HTML - CSS3 - Tailwind - Javascript - React - Firebase - Git -
           Figma
         </span>
       </h1>
